8ec43a0a70124198b6f9791abe49be0e82f88505
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-04-03 20:25:08 +0200

    docs: toast message component

--- src/app/shared/toast-message/toast-message.component.ts
+++ src/app/shared/toast-message/toast-message.component.ts
@@ -1,6 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ToastMessageService } from '../../../services/toast-message.service';
 
+/**
+ * Displays a toast message.
+ * 
+ * Together with the logic from ToastMessageService, the component is structured in such a way
+ * that only a single instance is required in the app. For the toast message to reload and for
+ * the toast message overlay to disappear, the component instance requires a condition to check
+ * if a message is defined.
+ * 
+ * Example of usage (where "toastMsg" is the ToastMessageService injection):
+ * 
+ * <app-toast-message *ngIf="toastMsg.message" [message]="toastMsg.message" />
+ */
 @Component({
   selector: 'app-toast-message',
   standalone: true,
@@ -12,11 +24,19 @@ export class ToastMessageComponent implements OnInit {
   @Input({ alias: 'message', required: true }) msg!: string;
   timeoutId?: ReturnType<typeof setTimeout>;
 
+
+  /**
+   * Constructor for injection of services.
+   */
   constructor(
     private toastMsg: ToastMessageService
   ) { }
 
 
+  /**
+   * Initializes timeout to automatically close the message. Saves timeout ID to check if the
+   * timeout still belongs to this respective toast when the timeout is over.
+   */
   ngOnInit(): void {
     const timeoutId = setTimeout(() => {
       if(this.timeoutId && timeoutId === this.timeoutId) {
@@ -27,6 +47,10 @@ export class ToastMessageComponent implements OnInit {
   }
 
 
+  /**
+   * Closes the toast message by setting the message value to undefined.
+   * Cleans up the component.
+   */
   close(): void {
     clearTimeout(this.timeoutId);
     this.timeoutId = undefined;
