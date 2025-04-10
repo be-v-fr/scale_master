fdd9d0e0435cba25c077590f2f0c411c034b3968
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-04-10 14:50:47 +0200

    feat: completed mode naming dialog

--- src/app/dialogs/dialog-name-mode/dialog-name-mode.component.ts
+++ src/app/dialogs/dialog-name-mode/dialog-name-mode.component.ts
@@ -6,6 +6,7 @@ import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { parseNumberParamIfExists } from '../../../utils/router.utils';
+import { DialogService } from '../../../services/dialog.service';
 
 @Component({
   selector: 'app-dialog-name-mode',
@@ -15,18 +16,21 @@ import { parseNumberParamIfExists } from '../../../utils/router.utils';
   styleUrl: './dialog-name-mode.component.scss'
 })
 export class DialogNameModeComponent implements OnInit, OnDestroy {
+  modeInterval?: number;
   modeName?: string;
   routeSub: Subscription = new Subscription();
 
   constructor(
     private route: ActivatedRoute,
-    private currScale: CurrentScaleService
+    private currScale: CurrentScaleService,
+    public dialog: DialogService
   ) { }
 
   ngOnInit(): void {
     this.routeSub = this.route.params.subscribe(params => {
       const interval: number | undefined = parseNumberParamIfExists(params, 'interval');
       if (typeof(interval) === 'number') {
+        this.modeInterval = interval;
         this.initModeName(interval);
       } else {
         console.error('Initialization failed because the mode interval was missing or invalid.');
@@ -41,12 +45,20 @@ export class DialogNameModeComponent implements OnInit, OnDestroy {
 
   initModeName(interval: number): void {
     const modeOriginal: ScaleMode | undefined = this.currScale.scale.getMode(interval);
-    console.log(modeOriginal);
     if (modeOriginal) {
       const modeClone: ScaleMode = cloneDeep(modeOriginal);
       this.modeName = modeClone.name;
     } else {
       console.error('Initialization failed because the mode could not be found in the scale category.');
     }
+  }
+
+  submit(): void {
+    if(!this.modeName || !(typeof(this.modeInterval) === 'number')) return;
+    const modeOriginal: ScaleMode | undefined = this.currScale.scale.getMode(this.modeInterval);
+    if (modeOriginal) {
+      modeOriginal.name = this.modeName;
+    }
+    this.dialog.close();
   }
 }
