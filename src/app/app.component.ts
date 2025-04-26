83a06a574c05fd8905d058a46f8d866348edd830
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-04-26 13:36:19 +0200

    style: general color scheme overhaul

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,6 @@ export class AppComponent implements OnInit {
 
 
   ngOnInit(): void {
-    setTimeout(() => this.awaitingIntro = false, 3200);
+    setTimeout(() => this.awaitingIntro = false, 3000);
   }
 }
