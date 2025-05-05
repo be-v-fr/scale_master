aa87141884dacd9a2bd98e416beee07baf80349a
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-05-05 20:27:12 +0200

    style: info message

--- src/utils/router.utils.ts
+++ src/utils/router.utils.ts
@@ -1,4 +1,4 @@
-import { Params, Router } from "@angular/router";
+import { Params } from "@angular/router";
 
 export function parseNumberParamIfExists(params: Params, key: string): number | undefined {
     if(typeof params[key] === 'string') return parseInt(params[key], 10);
