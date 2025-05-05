518f534f0af13cebcecd3f2117baa30302bc3ceb
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-05-05 18:45:26 +0200

    cleaning: unused function

--- src/utils/router.utils.ts
+++ src/utils/router.utils.ts
@@ -4,12 +4,3 @@ export function parseNumberParamIfExists(params: Params, key: string): number | 
     if(typeof params[key] === 'string') return parseInt(params[key], 10);
     return undefined;
 }
-
-export function updateEditRouteStep(mainUrl: string, step: number): string {
-    const urlSegments: (string | number)[] = mainUrl.split('/');
-    const editIndex: number = urlSegments.findIndex(s => s === 'edit');
-    if (editIndex >= 0) {
-      urlSegments[editIndex + 1] = step;
-    }
-    return urlSegments.join('/');
-}
