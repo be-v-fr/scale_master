4941d9aca6f63f124b3e163ec81dc9fddcdbd014
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-05-06 16:55:02 +0200

    feat: individual content sizing in scrollable list item including limit and ellipsis

--- src/app/shared/scrollable-list/scrollable-list-item/scrollable-list-item.component.ts
+++ src/app/shared/scrollable-list/scrollable-list-item/scrollable-list-item.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { DisplayService } from '../../../../services/display.service';
 
 /**
@@ -12,14 +12,50 @@ import { DisplayService } from '../../../../services/display.service';
   templateUrl: './scrollable-list-item.component.html',
   styleUrl: './scrollable-list-item.component.scss'
 })
-export class ScrollableListItemComponent {
-  @Input({ required: true }) content!: string | number;
+export class ScrollableListItemComponent implements AfterViewInit {
+  private _content!: string | number;
+  get content(): string | number {
+    return this._content;
+  }
+  @Input({ required: true }) set content(value: string | number) {
+    this._content = value;
+    this.cdr.detectChanges();
+    this.updateSize();
+  };
+
   @Input() default?: string | number;
   @ViewChild('contentElement') contentRef!: ElementRef<HTMLSpanElement>
+  scaleDown?: number;
+  get scaleDownLimit(): number {
+    return 0.75;
+  }
+  textEllipsis: boolean = false;
 
-  // TODO: Check width compared to parent. Reduce font size if too large.
 
   constructor(
-    public display: DisplayService
+    public display: DisplayService,
+    private el: ElementRef,
+    private cdr: ChangeDetectorRef
   ) { }
+
+
+  ngAfterViewInit(): void {
+    this.updateSize();
+  }
+
+
+  updateSize(): void {
+    const visibleWidth: number = this.el.nativeElement.offsetWidth / 2;
+    const contentWidth: number = this.contentRef.nativeElement.offsetWidth;
+    const widthRatio: number = contentWidth / visibleWidth;
+    if (widthRatio > 1) {
+      if (widthRatio > 1 / this.scaleDownLimit) {
+        this.textEllipsis = true;
+        this.scaleDown = this.scaleDownLimit;
+      } else {
+        this.scaleDown = 1 / widthRatio;
+      }
+      this.cdr.detectChanges();
+    }
+  }
 }
