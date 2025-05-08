044a21f92cb30006d1daf4f5f060a1db9a9e293e
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-05-08 13:10:39 +0200

    fix: imports

--- src/app/menu/main-headline-bar/main-headline-bar.component.ts
+++ src/app/menu/main-headline-bar/main-headline-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DisplayService } from '../../../services/display.service';
 import { CircularButtonComponent } from '../../shared/circular-button/circular-button.component';
 import { CurrentScaleService } from '../../../services/current-scale.service';
@@ -10,7 +11,7 @@ import { capitalizeFirstLetter } from '../../../utils/string.utils';
 @Component({
   selector: 'app-main-headline-bar',
   standalone: true,
-  imports: [CircularButtonComponent, RouterLink],
+  imports: [CommonModule, CircularButtonComponent, RouterLink],
   templateUrl: './main-headline-bar.component.html',
   styleUrl: './main-headline-bar.component.scss'
 })
